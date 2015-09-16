2d46b5e866750269d96b2c9d63e3445dafcd1599
mechevarria14 <mechevarria@chicagotechacademy.org>
2015-09-16 15:13:39 -0500

    Create assignment4.rb

---
+++ assignment4.rb
@@ -0,0 +1,7 @@
+puts "how many hours do you work?"
+hours = gets.chomp.to_i
+puts "how much money do you make per hour?"
+money = gets.chomp.to_i
+puts "how many weeks did you work?"
+weeks = gets.chomp.to_i
+total = hours*money*weeks
